94aae59c3f3c62d2f2d3ddf528a7c6eecd17957a
Babygungle <apredikanit@gmail.com>
2023-10-22 00:40:41 +0700

    Blue's work

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,27 @@
-class EmailNotification { }
+class EmailNotification { 
+    constructor (notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send () {
+        console.log(`Notification has been sent to ${this.notificationId}`)
+    }
+}
 
-class SMSNotification { }
+class SMSNotification { 
+    constructor (notificationId, createdTime, content, phoneNumber) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.phoneNumber = phoneNumber;
+    }
+    send () {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+const userEmail = new EmailNotification("2323AD@gmail.com", "Aug 10, 2023 10:09 AM", "test", "User B");
+const userSMS = new SMSNotification("2323AD@gmail.com", "Aug 10, 2023 10:10 AM", "test", "0834452564")
+userEmail.send()
+userSMS.send()
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,33 @@
-class Notification { }
+class Notification { 
+    constructor (notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send () {
+        console.log(`Notification has been sent to ${this.receiver}`)
+    }
+}
+class EmailNotification extends Notification { 
+    constructor (notificationId, createdTime, content, receiver) {
+        super(notificationId, createdTime, content, receiver)
+    }
+    send () {
+        console.log(`Notification has been sent to ${this.notificationId}`)
+    }   
+}
+class SMSNotification extends Notification { 
+    constructor (notificationId, createdTime, content, phoneNumber) {
+        super(notificationId, createdTime, content);
+        this.phoneNumber = phoneNumber;
+    }
+    send () {
+            console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+    }
 
-class EmailNotification { }
-
-class SMSNotification { }
+const userEmail = new EmailNotification("2323AD@gmail.com", "Aug 10, 2023 10:09 AM", "test", "User B");
+const userSMS = new SMSNotification("2323AD@gmail.com", "Aug 10, 2023 10:10 AM", "test", "0834452564")
+userEmail.send()
+userSMS.send()
